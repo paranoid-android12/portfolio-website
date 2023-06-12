51fe9788c558b65f682605225d887ea4cee04ee8
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2023-06-12 23:31:37 +0800

    Login page initial draft

--- Menu/script.js
+++ Menu/script.js
@@ -2,6 +2,6 @@ function clicked(){
     const title = document.getElementById("main");
     title.style.opacity = "0";
     setTimeout(function(){
-        window.open("../login/loginMain.html", "_self")
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", "_self")
     }, 2000);
 }
