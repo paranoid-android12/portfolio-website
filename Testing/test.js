eca89ac89039315684f122c8ecba6689ad2f41e3
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2023-06-15 23:11:02 +0800

    Initialized testing for MySQL connectivity

---
+++ Testing/test.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  con.query('SELECT * FROM')
+});
