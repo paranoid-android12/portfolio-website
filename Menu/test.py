9576e77f0d58f7fb50730db46cef3ed733116794
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2023-06-20 15:10:57 +0800

    slight adjustments on sizing

---
+++ Menu/test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+print(type(browser))
+browser.get('http://inventwithpython.com')
+
+try:
+    elem = browser.find_element_by_class_name('card-img-top cover-thumb')
+    print("Found <%s> element with that class name!" % (elem.tag_name))
+except:
+    print("Was not able to find an element with that name.")
